121684f464d9fe118e65a5e2463ea6c96a182b3e
Aniket Narvekar <aniket.narvekar0496@gmail.com>
2021-06-02 01:02:05 +0530

    Add api to fetch appropriate data from db server

--- index.js
+++ index.js
@@ -2,9 +2,29 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
 const port = 3000;
+const queries = require('./queries');
 
 app.use (bodyParser.json ());
 
 app.listen (port, () => {
     console.log (`App running on port ${port}.`);
 });
+
+app.get ('/api/metathesaurus/relation', async (request, response) => {
+    try {
+	let res = await queries.getRelation();
+	 response.status(200).json(res);
+    } catch (err) {
+	throw err;
+    }
+});
+
+app.get('/api/metathesaurus/concept/relation', async (request, response) => {
+    try {
+	let res = await queries.getConceptRelation(request.query.id);
+	response.status(200).json(res);
+    } catch (err) {
+	throw err;
+    }
+});
+
